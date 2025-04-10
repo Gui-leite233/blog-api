d268ae8b8baaeff6cce6245331a59a94572eea8c
Gui-leite233 <89623564+Gui-leite233@users.noreply.github.com>
2025-04-10 01:15:49 -0300

    error fixings

--- src/books/schemas/book.schema.ts
+++ src/books/schemas/book.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Schema as MongooseSchema } from 'mongoose';
+import { Category } from 'src/categories/schemas/category.schema';
 
 export type BookDocument = HydratedDocument<Book>;
 
@@ -16,6 +17,9 @@ export class Book {
 
   @Prop()
   publishedDate?: Date;
+
+  @Prop({type: [{type: MongooseSchema.Types.ObjectId, ref: 'Category'}]})
+  categories: Category[];
 }
 
 export const BookSchema = SchemaFactory.createForClass(Book);
